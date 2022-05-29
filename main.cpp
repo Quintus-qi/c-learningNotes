2a77b728d861510cbbd1af58284a4a90c2d4dd7f
qluo <chen.luoqi@foxmail.com>
2022-05-29 11:14:39 +0800

    qluo

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+* @ Created by qluo on 2022/5/28.
+* @ email chen.luoqi@foxmail.com
+* @ for c_learningNotes
+*/
+//
+
+#include "main.h"
+
+
+
---
+++ main.h
@@ -0,0 +1,16 @@
+/*
+* @ Created by qluo on 2022/5/28.
+* @ email chen.luoqi@foxmail.com
+* @ for c_learningNotes
+*/
+
+#ifndef C_LEARNINGNOTES_MAIN_H
+#define C_LEARNINGNOTES_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //C_LEARNINGNOTES_MAIN_H
